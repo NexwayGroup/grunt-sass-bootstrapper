1d9a6e13297d1828b139da4390cc062021d17f25
Michal Katanski <mkatanski@nexway.com>
2014-12-18 16:52:38 +0100

    Fixed bug with imports removing Fixed bug with regex pattern Added simply excludes Some code refactoring

--- tasks/sass_bootstrapper.js
+++ tasks/sass_bootstrapper.js
@@ -11,24 +11,24 @@
 var Path    = require('path'),
     Fs      = require('fs');
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     'use strict';
 
-    var options             = null,
-        PATH_SEP            = '/',
-        NEW_LINE            = '\n',
-        MODULE_NAME         = 'sass_bootstrapper',
-        MODULE_DESC         = 'Resolves dependency for SASS files and creates bootstrap file',
-        mainPartialList     = {};
+    var options         = null,
+        PATH_SEP        = '/',
+        NEW_LINE        = '\n',
+        MODULE_NAME     = 'sass_bootstrapper',
+        MODULE_DESC     = 'Resolves dependency for SASS files and creates bootstrap file',
+        mainPartialList = {};
 
     /**
      * Convert file path into sass format
      *
      * @returns {string} sass formated path
      */
-    String.prototype.toSassFormat = function() {
-        var _dir        = Path.dirname(this),
-            _fileName   = Path.basename(this);
+    String.prototype.toSassFormat = function () {
+        var _dir = Path.dirname(this),
+            _fileName = Path.basename(this);
 
         if (_fileName.indexOf('_') === 0) {
             _fileName = _fileName.replace('_', '');
@@ -42,7 +42,7 @@ module.exports = function(grunt) {
      *
      * @returns {string} sass formated path
      */
-    String.prototype.sassToPath = function() {
+    String.prototype.sassToPath = function () {
         var _index = this.lastIndexOf(PATH_SEP) + 1;
         return [this.slice(0, _index), '_', this.slice(_index)].join('');
     };
@@ -54,7 +54,7 @@ module.exports = function(grunt) {
      *
      * @returns {string} Parsed root folder
      */
-    String.prototype.parseRootFolder = function() {
+    String.prototype.parseRootFolder = function () {
         var _parts = this.split(PATH_SEP);
         if (_parts.length > 1) {
             return _parts[0];
@@ -68,11 +68,12 @@ module.exports = function(grunt) {
      * @rootFolders {array} Root folders to remove from path
      * @returns {string} Cleared path
      */
-    String.prototype.removeRootFolders = function(rootFolders) {
+    String.prototype.removeRootFolders = function (rootFolders) {
         var _this   = this,
             _return = '';
-        rootFolders.forEach(function(rootFolder) {
-            if(rootFolder.substr(-1) != PATH_SEP) {
+
+        rootFolders.forEach(function (rootFolder) {
+            if (rootFolder.substr(-1) != PATH_SEP) {
                 rootFolder += PATH_SEP;
             }
             var cleared = _this.replace(rootFolder, '');
@@ -91,7 +92,7 @@ module.exports = function(grunt) {
      * @param callback {function} Callback function to test element
      * @returns {number} Index of eleement or -1 if not found
      */
-    function findIndexInArray(array, callback){
+    function findIndexInArray(array, callback) {
         var _length = array.length;
         for (var i = 0; i < _length; i++) {
             if (callback(array[i])) return i;
@@ -122,7 +123,7 @@ module.exports = function(grunt) {
 
             for (var index in _imports) {
                 var _partialToRemove = _imports[index];
-
+                var i = 0;
                 // remove file from list
                 delete mainPartialList[_partialToRemove];
             } // end in imports
@@ -135,11 +136,9 @@ module.exports = function(grunt) {
      * @returns {Array} Array of partials
      */
     function getArrayOfPartials() {
-        var _partialsArr  = [];
-        for (var file in mainPartialList)
-        {
-            if (mainPartialList.hasOwnProperty(file))
-            {
+        var _partialsArr = [];
+        for (var file in mainPartialList) {
+            if (mainPartialList.hasOwnProperty(file)) {
                 _partialsArr.push(file);
             }
         }
@@ -155,8 +154,8 @@ module.exports = function(grunt) {
      * @returns {Object} list of all founded import values
      */
     function parseFile(importKeyword, requiresKeyword, filePath) {
-        var _lines   = grunt.file.read(filePath).split(NEW_LINE),
-            _values  = {
+        var _lines  = grunt.file.read(filePath).split(NEW_LINE),
+            _values = {
                 imports: {},
                 requires: {}
             };
@@ -167,7 +166,7 @@ module.exports = function(grunt) {
 
             if (line.indexOf(importKeyword) !== -1) {
                 // get import value
-                var _importValue = line.match(/"(.*?)"/)[1];
+                var _importValue = line.match(/("|')(.*?)("|')/)[2];
 
                 // Create full path to help looking for file and minimalize
                 // errors if there are duplicated filenames
@@ -180,7 +179,7 @@ module.exports = function(grunt) {
 
             if (line.indexOf(requiresKeyword) !== -1) {
                 // get require value
-                var _requireValue = line.match(/"(.*?)"/)[1];
+                var _requireValue = line.match(/("|')(.*?)("|')/)[2];
 
                 // Create new dependency object
                 var _requireObject = {
@@ -203,56 +202,69 @@ module.exports = function(grunt) {
      * @returns {array} reordered array of partials
      */
     function orderByDependency(partialsArray) {
-        var _doPass          = true,
-            _reqCache        = {};
+        var _doPass     = true,
+            _reqCache   = {};
         // repeat sorting operation until
         // there is no replacing procedure during
         // one pass (bubble sorting).
         while (_doPass) {
             _doPass = false;
+
             // Iterate through every partial in the list and check its
             // dependency (requirements)
-            partialsArray.forEach(function(partialName, partialIndex) {
-                var _requiredObject = mainPartialList[partialName].requires;
+            partialsArray.forEach(function (partialName, partialIndex) {
+                // Get require list for current partial
+                var _requireList = mainPartialList[partialName].requires;
 
-                for (var rKey in _requiredObject)
-                {
-                    if (_requiredObject.hasOwnProperty(rKey))
-                    {
-                        var _cacheFilename = mainPartialList[partialName].removedRoot,
-                            _requiredIndex = findIndexInArray(partialsArray, function(partialName){
-                                return (partialName.match(_requiredObject[rKey].requires) !== null);
-                            });
+                // for each requirement in require object do...
+                for (var rKey in _requireList) {
 
-                        if (_reqCache[_requiredObject[rKey].requires + _cacheFilename]) {
-                            // require loop detected, throw an error and break program
-                            grunt.fail.fatal(
-                                'Require loop detected!!! \nFiles that have require loop: \n' +
-                                mainPartialList[partialsArray[_requiredIndex]].fileName + '\n' +
-                                _requiredObject[rKey].fileName
-                            );
-                        }
+                    // Check if current key is _requireList property
+                    if (!_requireList.hasOwnProperty(rKey)) {
+                        // if not go to next iteration
+                        continue;
+                    }
 
-                        if (_requiredIndex > -1 && _requiredIndex > partialIndex) {
-                            // Remove partial from current index and insert
-                            // it before partial that requires it.
-                            partialsArray = replaceInArray(partialsArray, _requiredIndex, partialIndex);
-                            // Replacing procedure occured so do one more pass.
-                            _doPass = true;
-                            // add requirement to cache for requirement loop handling
-                            _reqCache[_cacheFilename + _requiredObject[rKey].requires] = true;
-                        } else  if (_requiredIndex === -1) {
-                            // invalid requirement, throw a warning
-                            grunt.log.warn(
-                                'File: ' + mainPartialList[partialName].fileName +
-                                ' is depending on non existing partial "' +
-                                _requiredObject[rKey].requires +'"'
-                            );
-                        }
+                        // Current partial filename without root folder
+                    var _fileName = mainPartialList[partialName].removedRoot,
+                        // Index of current partial in partialsArray, -1 if not found.
+                        _requireIndex = findIndexInArray(partialsArray, function (partialName) {
+                            return (partialName.match(_requireList[rKey].requires) !== null);
+                        }),
+                        _requireObject = _requireList[rKey];
 
-                    } // end if
+                    // Check require cache for dependency loop
+                    if (_reqCache[_requireObject.requires + _fileName]) {
+                        // dependency loop detected, throw an error and break program
+                        grunt.fail.fatal(
+                            'Dependency loop detected!!! \nFiles that have dependency loop: \n' +
+                            mainPartialList[partialsArray[_requireIndex]].fileName + '\n' +
+                            _requireObject.fileName
+                        );
+                    }
+
+                    // If requirement is found and its position is incorrect
+                    if (_requireIndex > -1 && _requireIndex > partialIndex) {
+                        // Remove partial from current index and insert
+                        // it before partial that requires it.
+                        partialsArray = replaceInArray(partialsArray, _requireIndex, partialIndex);
+                        // Replacing procedure occured so do one more pass.
+                        _doPass = true;
+                        // add requirement to require cache for dependency loop handling
+                        _reqCache[_fileName + _requireObject.requires] = true;
+
+                    } else if (_requireIndex === -1) {
+                        // invalid requirement, throw a warning
+                        grunt.log.warn(
+                            'File: ' + mainPartialList[partialName].fileName +
+                            ' is depending on non existing partial "' +
+                            _requireObject.requires + '"'
+                        );
+                    }
+
                 } // end for
             }); // end for each
+
         } // end while
         // Return reordered file list
         return partialsArray;
@@ -266,18 +278,18 @@ module.exports = function(grunt) {
      * @returns {string} formatted file content
      */
     function generateBootstrapContent(partialsArray, lineDelimiter) {
-        var _bootstrapContent    = '',
-            _folderGroup         = '';
+        var _bootstrapContent   = '',
+            _folderGroup        = '';
 
-        partialsArray.forEach(function(partialName) {
-            var _sassPath    = mainPartialList[partialName].removedRoot.toSassFormat(),
+        partialsArray.forEach(function (partialName) {
+            var _sassPath = mainPartialList[partialName].removedRoot.toSassFormat(),
                 _currentRoot = _sassPath.parseRootFolder();
 
             // if current root folder has changed, write new root folder
             // as a comment and set new current folder
             if (_folderGroup != _currentRoot) {
-                _bootstrapContent   += NEW_LINE + '// ' + _currentRoot + NEW_LINE;
-                _folderGroup         = _currentRoot;
+                _bootstrapContent += NEW_LINE + '// ' + _currentRoot + NEW_LINE;
+                _folderGroup = _currentRoot;
             }
             // append import line
             _bootstrapContent += options.importKeyword + ' "' + _sassPath + '"' + lineDelimiter + NEW_LINE;
@@ -289,24 +301,25 @@ module.exports = function(grunt) {
     /**
      * Grunt task main function
      */
-    grunt.registerMultiTask(MODULE_NAME, MODULE_DESC, function() {
+    grunt.registerMultiTask(MODULE_NAME, MODULE_DESC, function () {
 
         // Merge task-specific and/or target-specific options with these defaults.
         options = this.options({
             filterRootPaths:    ['app/styles/', 'bower_components/'],
             bootstrapFile:      'app/styles/bootstrap.sass',
-            requireKeyword:     '#requires'
+            requireKeyword:     '#requires',
+            exclude: []
         });
 
-        var _lineDelimiter       = ';',
-            _bootstrapContent    = '// This file has been generated by grunt-sass-bootstrapper. \n' +
-                '// A SASS/SCSS Dependency resolver module.\n\n' +
-                '// Please do not edit this file!\n',
-            _cachedPartialList   = {},
-            _parsingCount        = 0;
+        var _lineDelimiter      =   ';',
+            _bootstrapContent   =   '// This file has been generated by grunt-sass-bootstrapper. \n' +
+                                    '// A SASS/SCSS Dependency resolver module.\n\n' +
+                                    '// Please do not edit this file!\n',
+            _cachedPartialList  =   {},
+            _parsingCount       =   0;
 
-        options.importKeyword   = '@import';
-        options.cacheFile       = '.cache/'+MODULE_NAME+'/cache.json';
+        options.importKeyword   =   '@import';
+        options.cacheFile       =   '.cache/' + MODULE_NAME + '/cache.json';
 
         if (options.importKeyword === options.requireKeyword) {
             grunt.fail.fatal('Using "@import" as requireKeyword name is depracated');
@@ -328,10 +341,10 @@ module.exports = function(grunt) {
         }
 
         // Iterate over all specified file groups.
-        this.files.forEach(function(fGroup) {
+        this.files.forEach(function (fGroup) {
 
             // Insert all files into array.
-            fGroup.src.filter(function(filePath) {
+            fGroup.src.filter(function (filePath) {
                 // Warn on and remove invalid source files (if nonull was set).
                 if (!grunt.file.exists(filePath)) {
                     grunt.log.warn('Source file "' + filePath + '" not found.');
@@ -343,8 +356,8 @@ module.exports = function(grunt) {
                         _partialObject  = {
                             lastModified:   new Date(Fs.lstatSync(filePath).mtime).getTime(),
                             fileName:       filePath,
-                            imports: {},
-                            requires: {}
+                            imports:        {},
+                            requires:       {}
                         };
 
                     // check if cached data for current file exists
@@ -358,7 +371,7 @@ module.exports = function(grunt) {
                             _partialObject.requires     = _cachedPartial.requires;
                             _partialObject.removedRoot  = _cachedPartial.removedRoot;
                             // disable parsing for current file
-                            _doParse                    = false;
+                            _doParse = false;
                         }
                     }
 
@@ -366,15 +379,17 @@ module.exports = function(grunt) {
                         // Parse file
                         var _parsedData = parseFile(options.importKeyword, options.requireKeyword, filePath);
                         // collect imports and requirements
-                        _partialObject.imports  = _parsedData.imports;
+                        _partialObject.imports = _parsedData.imports;
                         _partialObject.requires = _parsedData.requires;
                         _parsingCount++;
                         // Remove root paths
                         _partialObject.removedRoot = _noExtension.removeRootFolders(options.filterRootPaths);
                     }
 
-                    // Append new element to main partials list
-                    mainPartialList[_noExtension] = _partialObject;
+                    // Append new element to main partials list if its not listed in exclude array
+                    if (options.exclude.indexOf(_partialObject.fileName) === -1) {
+                        mainPartialList[_noExtension] = _partialObject;
+                    }
 
                     return true;
                 }
@@ -386,7 +401,7 @@ module.exports = function(grunt) {
             grunt.file.write(options.cacheFile, JSON.stringify(mainPartialList, null, 4));
         }
 
-        clearImports(mainPartialList);
+        clearImports(JSON.parse(JSON.stringify(mainPartialList)));
         var _orderedPartialsArray = orderByDependency(getArrayOfPartials());
 
         _bootstrapContent += generateBootstrapContent(_orderedPartialsArray, _lineDelimiter);
@@ -396,8 +411,6 @@ module.exports = function(grunt) {
 
         // Print a success message.
         grunt.log.writeln('File "' + options.bootstrapFile + '" created.');
-
-
 
     });
 };
